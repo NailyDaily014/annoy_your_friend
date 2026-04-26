4f0304be6ae2f57a7f90f0e0d1f01fbc09fd0f2e
NailyDaily014 <nailislamov014@gmail.com>
2026-04-26 11:25:05 -0500

    Updated project structure + added new services

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import requests
+from services import urls
+PHONE = "79276388363"
+for i in range(1):
+    url_list = urls(PHONE)
+    for service in url_list:
+        try:
+            body = service.get('json') or service.get('data')
+            response = requests.request(
+                method=service.get('method'),
+                url=service['url'],
+                headers=service.get('headers'),
+                cookies=service.get('cookies'),
+                data=body if 'data' in service and not 'json' in service else None,
+                json=body if 'json' in service else None,
+                timeout=5
+            )
+            print(response.status_code)
+            if len(response.text)<200:
+                print(response.text)
+        except:
+            print("Error")
